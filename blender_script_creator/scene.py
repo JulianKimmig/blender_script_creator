92c49cf4de4cd513fe89e51d9d6a501646f2ccd8
JulianKimmig <5rK0vVjNYRxx176qP15R>
2020-10-08 10:59:01 +0200

    Lightsource

--- blender_script_creator/scene.py
+++ blender_script_creator/scene.py
@@ -1,11 +1,19 @@
 from blender_script_creator import bpy
-from blender_script_creator.geometry import BlenderObject
+from blender_script_creator.geometry import BlenderObject, create_plain_object
 from blender_script_creator.materials import Material
 from blender_script_creator.script import blender_function, blender_basic_script
-
+import numpy as np
 
 @blender_function(dependencies=[BlenderObject,Material])
 def delete_all_but(l=[]):
+    def flatten(ll):
+        if isinstance(ll,(list,tuple,np.ndarray)):
+            ll=list(ll)
+        else:
+            return [ll]
+        return flatten(ll[0]) + (flatten(ll[1:]) if len(ll) > 1 else [])
+    l = list(flatten(l))
+
     obj_list = []
     def append_blender_object(obj):
         obj_list.append(obj)
@@ -27,7 +35,6 @@ def delete_all_but(l=[]):
         else:
             print(i)
             obj_list.append(i)
-    #print(obj_list)
     obj_list=list(set(obj_list))
 
     bpy.ops.object.select_all(action='DESELECT')
@@ -38,10 +45,49 @@ def delete_all_but(l=[]):
 
     for material in bpy.data.materials:
         if material not in obj_list:
-            material.user_clear()
-            bpy.data.materials.remove(material)
+            if material.users == 0:
+                bpy.data.materials.remove(material)
+
+    for mesh in bpy.data.meshes:
+        if mesh not in obj_list:
+            if mesh.users == 0:
+                bpy.data.meshes.remove(mesh)
+
+    for curve in bpy.data.curves:
+        if curve not in obj_list:
+            if curve.users == 0:
+                bpy.data.curves.remove(curve)
 
 @blender_function(dependencies=[BlenderObject])
 def store_scene(name):
     for n,obj in BlenderObject.objects.items():
         obj.store_scene(name)
+
+
+class LightSource(BlenderObject):
+    def __init__(self, obj, name,type="POINT",energy=10):
+        super().__init__(obj, name)
+        self.type = type
+        self.energy=energy
+
+    @property
+    def type(self):
+        return self._obj.data.type
+
+    @type.setter
+    def type(self,type):
+        self._obj.data.type = type
+
+    @property
+    def energy(self):
+        return self._obj.data.energy
+
+    @energy.setter
+    def energy(self,energy):
+        self._obj.data.energy = energy
+
+    @classmethod
+    def new(cls,name,type='POINT',**kwargs):
+        light_data = bpy.data.lights.new(name=name, type=type)
+        light_data.energy = 10
+        return LightSource(create_plain_object(name, light_data),name,type=type, **kwargs)
