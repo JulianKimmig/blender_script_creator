77b069bcc7e29c5c1b6175bfbca43a61a7145685
JulianKimmig <5rK0vVjNYRxx176qP15R>
2020-10-08 11:02:02 +0200

    special keyframes

--- blender_script_creator/animation.py
+++ blender_script_creator/animation.py
@@ -20,6 +20,8 @@ class BlenderAnimationTracker(BlenderClass):
         return self._max_frame
 
     def go_to_frame(self, f):
+        if isinstance(f,str):
+            f = self.get_frame(f)
         self._max_frame = max(self._max_frame, f)
         self.current_frame = f
 
@@ -27,9 +29,13 @@ class BlenderAnimationTracker(BlenderClass):
         self.go_to_frame(self.current_frame + np.ceil(f))
 
     def seconds_to_frames(self, s):
+        if s is None:
+            return None
         return self._fps * s
 
     def frames_to_seconds(self, f):
+        if f is None:
+            return None
         return f / self._fps
 
     def go_to_second(self, s):
@@ -38,7 +44,7 @@ class BlenderAnimationTracker(BlenderClass):
     def run_seconds(self, s):
         self.run_frames(self.seconds_to_frames(s))
 
-    def finish_animation(self,start=None,end=None):
+    def finish_animation(self,start=None,end=None,current=None):
         if start is None:
             start = 0
         if isinstance(start,str):
@@ -47,18 +53,33 @@ class BlenderAnimationTracker(BlenderClass):
             end = self._max_frame
         if isinstance(end,str):
             end = self.get_frame(end)
+
+        if current is None:
+            current = start
+        if isinstance(current,str):
+            current = self.get_frame(current)
+
         bpy.context.scene.frame_start = start
         bpy.context.scene.frame_end = end
+        bpy.context.scene.frame_current = current
 
         for fh in self._finish_hooks:
             fh(self)
+
+        bpy.ops.screen.animation_play()
 
     def clear_all(self):
         for a in bpy.data.actions:
             bpy.data.actions.remove(a)
 
+
+    def insert_keyframe(self,obj,data_path):
+        return obj.keyframe_insert(data_path=data_path, frame=self.current_frame)
+
     def performe_keyframe_op(self, obj, data_path, frames=0, reverse=False, interpolation=None):
-        kf = obj.keyframe_insert(data_path=data_path, frame=self.current_frame)
+        if frames is None:
+            frames = 1
+        kf = self.insert_keyframe(obj,data_path=data_path)
 
         if interpolation is not None:
             action = obj.animation_data.action
@@ -88,6 +109,8 @@ class BlenderAnimationTracker(BlenderClass):
         return compl
 
     def move_object(self, obj, x=0, y=0, z=0, delta=False, time=0, reverse=False, interpolation=None):
+        if time is None:
+            time = self.frames_to_seconds(1)
         if isinstance(obj, BlenderObject):
             obj = obj.obj
         kfcb = self.performe_keyframe_op(obj, "location", frames=self.seconds_to_frames(time), reverse=reverse,
@@ -106,7 +129,27 @@ class BlenderAnimationTracker(BlenderClass):
 
         return delta
 
+    def bevel_start_end(self, obj,start=0,end=1, time=0, reverse=False, interpolation=None):
+        if time is None:
+            time = self.frames_to_seconds(1)
+        if isinstance(obj, BlenderObject):
+            obj = obj.obj
+
+        kfcb1 = self.performe_keyframe_op(obj.data, "bevel_factor_end", frames=self.seconds_to_frames(time), reverse=reverse,
+                                         interpolation=interpolation)
+        kfcb2 = self.performe_keyframe_op(obj.data, "bevel_factor_start", frames=self.seconds_to_frames(time), reverse=reverse,
+                                         interpolation=interpolation)
+
+        obj.data.bevel_factor_end = end
+        obj.data.bevel_factor_start = start
+
+        kfcb1()
+        kfcb2()
+
+
     def scale_object(self, obj, x=0, y=0, z=0, delta=False, time=0, reverse=False, interpolation=None):
+        if time is None:
+            time = self.frames_to_seconds(1)
         if isinstance(obj, BlenderObject):
             obj = obj.obj
         kfcb = self.performe_keyframe_op(obj, "scale", frames=self.seconds_to_frames(time), reverse=reverse,
@@ -126,6 +169,8 @@ class BlenderAnimationTracker(BlenderClass):
         return delta
 
     def move_objects(self, objs, x=0, y=0, z=0, delta=False, time=0, reverse=False, interpolation=None):
+        if time is None:
+            time = self.frames_to_seconds(1)
         delta =  self.move_object(objs[0],x=x,y=y,z=z,delta=delta,time=time,reverse=True,interpolation=interpolation)
 
         if len(objs)>1:
@@ -138,6 +183,8 @@ class BlenderAnimationTracker(BlenderClass):
 
 
     def rotate_object(self, obj, x=0, y=0, z=0, delta=False, animator=None, time=0, reverse=False, interpolation=None):
+        if time is None:
+            time = self.frames_to_seconds(1)
         if isinstance(obj, BlenderObject):
             obj = obj.obj
         kfcb = self.performe_keyframe_op(obj, "rotation_euler", frames=self.seconds_to_frames(time), reverse=reverse,
@@ -218,6 +265,13 @@ class Camera(BlenderObject):
         self.lens = lens
         if bpy.context.scene.camera is None:
             self.set_active_cam()
+        self._tracker={}
+        for name, tr in self.obj.constraints.items():
+            if tr.type == "TRACK_TO":
+                if tr.target in self._tracker:
+                     self.obj.constraints.remove(tr)
+                else:
+                    self._tracker[tr.target]=tr
 
     def set_active_cam(self):
         bpy.context.scene.camera = self.obj
@@ -236,3 +290,41 @@ class Camera(BlenderObject):
         cam_data = bpy.data.cameras.new(name)
         cam = Camera(create_plain_object(name, cam_data), name, **kwargs)
         return cam
+
+    def stop_tracking(self,animator=None,frames=None):
+        for ob,tr in self._tracker.items():
+            if tr.influence != 0:
+                if animator:
+                    cb = animator.performe_keyframe_op(tr,"influence",frames=frames,reverse=True)
+                tr.influence = 0
+                if animator:
+                    cb()
+
+
+    def track(self,obj,influence=1,stop_other=True,animator=None,frames=None):
+        if isinstance(obj,BlenderObject):
+            obj=obj.obj
+
+
+        if obj not in self._tracker:
+            obj_tracker = self.obj.constraints.new(type='TRACK_TO')
+            self._tracker[obj] = obj_tracker
+            obj_tracker.target = obj
+            self._tracker[obj].influence = 0
+            if animator:
+                cf=animator.current_frame
+                animator.go_to_frame(0)
+                cb = animator.performe_keyframe_op(self._tracker[obj],"influence",frames=None,reverse=True)
+                animator.go_to_frame(cf)
+
+
+        if stop_other:
+            own=self._tracker[obj].influence
+            self.stop_tracking(animator=animator,frames=frames)
+            self._tracker[obj].influence = own
+
+        if animator:
+            cb = animator.performe_keyframe_op(self._tracker[obj],"influence",frames=frames,reverse=True)
+        self._tracker[obj].influence = influence
+        if animator:
+            cb()
